51d4d7f27d1cf1437c6096667ba2df7fb0f3e69d
Audrow Nash <audrow@hey.com>
2022-03-10 18:37:42 -0800

    Fix delete
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/plugins/db-plugins.ts
+++ src/plugins/db-plugins.ts
@@ -1,7 +1,8 @@
 import {REPOSITORY} from '../constants.github'
 import {
-  closeIssueByTitleAndLabels,
+  closeIssue,
   createIssues,
+  getIssues,
 } from '../test-cases/db/github/gh-issues'
 import testCaseToGithubIssue from '../test-cases/db/github/test-case-to-gh-issue'
 import loadMatchingTestCases from '../test-cases/utils/load-matching-test-cases'
@@ -15,14 +16,21 @@ const dbPlugins: DbPlugins = {
       await createIssues(REPOSITORY, issues)
     },
     delete: async (regex: string) => {
-      const issues = loadMatchingTestCases(regex).map(testCaseToGithubIssue)
+      const issues = (await getIssues(REPOSITORY, 'open')).filter((issue) => {
+        const nameMatch = issue.title.match(regex)
+        const labelMatch = issue.labels.some((label) => {
+          if (typeof label === 'string') {
+            return label.match(regex)
+          } else if (typeof label.name === 'string') {
+            return label.name.match(regex)
+          } else {
+            return false
+          }
+        })
+        return nameMatch || labelMatch
+      })
       issues.forEach(
-        async (issue) =>
-          await closeIssueByTitleAndLabels(
-            REPOSITORY,
-            issue.title,
-            issue.labels,
-          ),
+        async (issue) => await closeIssue(REPOSITORY, issue.number),
       )
     },
   },
