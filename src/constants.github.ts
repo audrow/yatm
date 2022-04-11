005947a0b3cf67e87920b4d410421195a370ea5d
Audrow Nash <audrow@hey.com>
2022-04-11 08:08:55 -0700

    Update the repo to push to
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/constants.github.ts
+++ src/constants.github.ts
@@ -6,6 +6,6 @@ export const GITHUB_API_TOKEN =
 export const RETRY_SECONDS = 60 * 5
 
 export const REPOSITORY: Repo = {
-  owner: 'audrow',
-  name: 'humble-tcm-example',
+  owner: 'osrf',
+  name: 'ros2_test_cases',
 }
