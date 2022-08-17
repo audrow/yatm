79dd42f0542d847a19cdb6a3bd9066e7e957da4a
Audrow Nash <audrow@hey.com>
2022-08-17 14:18:29 -0500

    Make Github API token consistent
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/constants.github.ts
+++ src/constants.github.ts
@@ -1,11 +1,11 @@
 import type Repo from './test-cases/db/github/__types__/Repo'
 
-export const GITHUB_API_TOKEN =
-  process.env.GITHUB_API_TOKEN || 'GITHUB_API_TOKEN is not set'
+export const GITHUB_TOKEN =
+  process.env.GITHUB_TOKEN || 'GITHUB_TOKEN is not set'
 
 export const RETRY_SECONDS = 60 * 5
 
 export const REPOSITORY: Repo = {
-  owner: 'osrf',
-  name: 'ros2_test_cases',
+  owner: 'audrow',
+  name: 'gazebo-test-cases',
 }
