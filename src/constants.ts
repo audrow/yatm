45c6ed312d26e7fc966c4d8943fbd6ecf761c641
Yadunund <yadunund@openrobotics.org>
2023-06-13 18:53:39 +0800

    Use zod to parse envars
    Signed-off-by: Yadunund <yadunund@openrobotics.org>

--- src/constants.ts
+++ src/constants.ts
@@ -1,33 +1,53 @@
 import 'dotenv/config'
 import fs from 'fs'
 import {join} from 'path'
+import {z} from 'zod'
 import type Repo from './test-cases/db/github/__types__/Repo'
 
 // =================================================================================================
-// Define the names of the environment variables that will be retireved.
-export const TEST_CASE_CONFIG_PATH_ENVAR_NAME = 'YATM_TEST_CASE_CONFIG_PATH'
-export const REQUIREMENTS_DIRECTORY_PATH_ENVAR_NAME =
-  'YATM_REQUIREMENTS_DIRECTORY_PATH'
-export const OUTPUT_DIRECTORY_PATH_ENVAR_NAME = 'YATM_OUTPUT_DIRECTORY_PATH'
+// Define the names of the environment variables that will be retrieved.
+const envSchema = z.object({
+  YATM_TEST_CASE_CONFIG_PATH: z.string(),
+  YATM_REQUIREMENTS_DIRECTORY_PATH: z.string(),
+  YATM_OUTPUT_DIRECTORY_PATH: z
+    .string()
+    .default(join(process.cwd(), 'generated-files')),
+  GITHUB_REPO_OWNER: z.string(),
+  GITHUB_REPO_NAME: z.string(),
+  GITHUB_TOKEN: z.string(),
+})
 
-export const GITHUB_REPO_OWNER_ENVAR_NAME = 'GITHUB_REPO_OWNER'
-export const GITHUB_REPO_NAME_ENVAR_NAME = 'GITHUB_REPO_NAME'
-export const GITHUB_TOKEN_ENVAR_NAME = 'GITHUB_TOKEN'
+const customErrorMap: z.ZodErrorMap = (issue, ctx) => {
+  if (issue.code === z.ZodIssueCode.invalid_type) {
+    if (issue.received === 'undefined') {
+      return {
+        message: 'Required environment variable ' + issue.path + ' not set.',
+      }
+    }
+  }
+  return {message: ctx.defaultError}
+}
+
+z.setErrorMap(customErrorMap)
+
 export const CODE_URL = 'https://github.com/audrow/yatm'
 
 // =================================================================================================
 // Retrieve the environment variables and define other constants to export.
-export const TEST_CASE_CONFIG = extractStringEnvVar(
-  TEST_CASE_CONFIG_PATH_ENVAR_NAME,
-)
-export const INPUT_REQUIREMENTS_PATH = extractStringEnvVar(
-  REQUIREMENTS_DIRECTORY_PATH_ENVAR_NAME,
-)
+const result = envSchema.safeParse(process.env)
 
-export const OUTPUT_PATH = extractStringEnvVar(
-  OUTPUT_DIRECTORY_PATH_ENVAR_NAME,
-  join(process.cwd(), 'generated-files'),
-)
+if (!result.success) {
+  for (const issue of result.error.issues) {
+    console.log(issue['message'])
+  }
+  process.exit(1)
+}
+
+const ENV = result.data
+
+export const TEST_CASE_CONFIG = ENV.YATM_TEST_CASE_CONFIG_PATH
+export const INPUT_REQUIREMENTS_PATH = ENV.YATM_REQUIREMENTS_DIRECTORY_PATH
+export const OUTPUT_PATH = ENV.YATM_OUTPUT_DIRECTORY_PATH
 export const OUTPUT_REQUIREMENTS_PATH = join(OUTPUT_PATH, 'requirements')
 export const OUTPUT_TEST_CASE_PATH = join(OUTPUT_PATH, 'test-cases')
 export const OUTPUT_TEST_CASE_RENDER_PATH = join(
@@ -35,31 +55,14 @@ export const OUTPUT_TEST_CASE_RENDER_PATH = join(
   'rendered-test-cases',
 )
 
-export const GITHUB_TOKEN = extractStringEnvVar(GITHUB_TOKEN_ENVAR_NAME)
+export const GITHUB_TOKEN = ENV.GITHUB_TOKEN
 export const GITHUB_RETRY_SECONDS = 60 * 5
 export const REPOSITORY: Repo = {
-  owner: extractStringEnvVar(GITHUB_REPO_OWNER_ENVAR_NAME),
-  name: extractStringEnvVar(GITHUB_REPO_NAME_ENVAR_NAME),
+  owner: ENV.GITHUB_REPO_OWNER,
+  name: ENV.GITHUB_REPO_NAME,
 }
 
 const packageXml = JSON.parse(
   fs.readFileSync(join(process.cwd(), 'package.json'), 'utf8'),
 )
 export const VERSION = packageXml.version
-
-// =================================================================================================
-// Helper function to retrieve environment variables or throw and error if unspecified.
-export function extractStringEnvVar(
-  key: keyof NodeJS.ProcessEnv,
-  default_value = '',
-): string {
-  const value = process.env[key] || default_value
-
-  if (value === undefined || value === '') {
-    const message = `The environment variable "${key}" is not set.".`
-
-    throw new Error(message)
-  }
-
-  return value
-}
